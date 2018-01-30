955ee269aea766281369ca1a247f4e0d4ade845f
qore <bastiaan.c+github@gmail.com>
2018-01-30 21:13:39 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,13 +2,12 @@
 function connectDB() {
 ## Database stuff
 global $db;
-$db = ($GLOBALS["___mysqli_ston"] = mysqli_connect('localhost', 'XXXXX', 'XXXX'));
-
+$db = ($GLOBALS["___mysqli_ston"] = mysqli_connect($_ENV['uri'], $_ENV['username'], $_ENV['password']));
 	if (!$db) {
 	die("Unable to connect to database");
 	}
 ##if (!mysql_select_db('spider')) {
-if (!mysqli_select_db($GLOBALS["___mysqli_ston"], "spider")) 
+if (!mysqli_select_db($GLOBALS["___mysqli_ston"], $_ENV['database_name'])) 
 {		
 	die("Unable to access spider database");
 	}
